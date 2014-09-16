610c60eecf36eaf5a7812bb3baa0d9087540b6fc
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2014-09-16 17:42:30 -0500

    Setting blog.hokanson.us

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.hokanson.us'
-SITEURL = 'http://jeffrey.github.io'
+SITEURL = 'http://blog.hokanson.us'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
